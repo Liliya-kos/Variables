78ed6d3d26b1c9bc4a7538506974c117a6289104
Liliya-kos <kosmakovaliliya@gmail.com>
2024-12-23 20:01:13 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,5 @@
+num_of_hometasks=12
+num_of_hours=1.5
+name_of_course='Python'
+one_task_hrs=num_of_hours/num_of_hometasks
+print('Курс:',name_of_course,', всего задач:',num_of_hometasks,', затрачено часов:',num_of_hours,', среднее время выполнения одного задания:',one_task_hrs)
